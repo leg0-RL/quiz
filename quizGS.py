3966874244813ebf8850592cc2cfd052824d6945
csPython <cspython@AD25TF04NGF1J-49.local>
2019-01-07 10:45:40 -0500

    Version 2, now has 2 questions

---
+++ quizGS.py
@@ -0,0 +1,40 @@
+print ("What is 1+1?")
+print ("")
+
+
+q1 = False
+score = 0
+rage = 0
+ppq = 100 #points per question
+
+while q1 == False:
+    try:
+        print("(1) 4")
+        print("(2) 3")
+        print("(3) 2")
+        print("(4) 1")
+        a1 = int(input("Your answer >"))
+        if 0 < a1 < 5:
+            q1 = True
+            if a1 == 3:
+                score += ppq
+                rage = 0
+            else:
+                score += 0
+                rage = 0
+        else:
+             rage += 1
+        if 0 < rage < 4:
+            print("Oh come on. You had one job. Answer 1,2,3, or 4. Try again")
+        if 3 < rage < 5:
+            print ("Oh come on! FFS! 1, 2, 3, or 4! Its not rocket science!")
+        if rage == 5:
+            print("Im done with you. Go do something else. You are clearly incapable of ansering a simple multiple chice question.")
+        if rage >= 6:
+            print("Stop. Please, PLEASE stop!")
+            
+    except ValueError:
+        print("Hey. Its a number. 1, 2, 3, or 4. Try again")
+
+
+print ("Nice job completing this super HARD test! Your score is", score,"%") 
